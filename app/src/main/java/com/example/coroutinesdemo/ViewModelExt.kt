0f492de43c104ff9df81f6e410bf63e5794b27c9
xulimin <xulimin@fang.com>
2020-12-15 10:04:50 +0800

    抽象出请求方法作为viewmodel的拓展函数，使用lamdba作为闭包传参

---
+++ app/src/main/java/com/example/coroutinesdemo/ViewModelExt.kt
@@ -0,0 +1,24 @@
+package com.example.coroutinesdemo
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+//viewmodel的拓展方法文件
+fun ViewModel.launch(
+    block:suspend CoroutineScope.()->Unit,
+    onError:(e: Throwable) -> Unit = {},
+    onComplete: () -> Unit = {}
+){
+    viewModelScope.launch(CoroutineExceptionHandler{_,e->
+        onError(e)
+    }){
+        try {
+            block.invoke(this)
+        }finally {
+            onComplete()
+        }
+    }
+}
--- app/src/main/java/com/example/coroutinesdemo/ui/MainViewModel.kt
+++ app/src/main/java/com/example/coroutinesdemo/ui/MainViewModel.kt
@@ -6,37 +6,59 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.coroutinesdemo.bean.LoadState
+import com.example.coroutinesdemo.launch
 import com.example.coroutinesdemo.network.NetworkService
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
-class MainViewModel():ViewModel() {
+class MainViewModel() : ViewModel() {
     //存放三张图片的url数据
     val imageData = MutableLiveData<List<String>>()
+
     //存放网路加载状态信息
     val loadState = MutableLiveData<LoadState>()
 
     //从网络加载数据
     fun getData() {
-        Log.d("xulimin","开始获取数据")
-        viewModelScope.launch (CoroutineExceptionHandler { _, e ->
-            //加载失败的状态
-            Log.d("xulimin","错误信息是"+e.message)
-            loadState.value = LoadState.Fail(e.message ?: "加载失败")
-        }){
-            //更新加载状态
+//        Log.d("xulimin","开始获取数据")
+//        viewModelScope.launch (CoroutineExceptionHandler { _, e ->
+//            //加载失败的状态
+//            Log.d("xulimin","错误信息是"+e.message)
+//            loadState.value = LoadState.Fail(e.message ?: "加载失败")
+//        }){
+//            //更新加载状态
+//            loadState.value = LoadState.Loading()
+//            //并发请求三张图片的数据
+//            val data1 = async { NetworkService.apiService.getImages() }
+//            val data2 = async { NetworkService.apiService.getImages() }
+//            val data3 = async { NetworkService.apiService.getImages() }
+//            //通过为LiveData设置新的值来触发更新UI
+//            imageData.value  = listOf(data1.await(), data2.await(), data3.await()).map { it.result[0].img }
+//
+//            //更新加载状态
+//            loadState.value = LoadState.Success()
+//
+//        }
+        launch({
             loadState.value = LoadState.Loading()
-            //并发请求三张图片的数据
             val data1 = async { NetworkService.apiService.getImages() }
             val data2 = async { NetworkService.apiService.getImages() }
             val data3 = async { NetworkService.apiService.getImages() }
             //通过为LiveData设置新的值来触发更新UI
-            imageData.value  = listOf(data1.await(), data2.await(), data3.await()).map { it.result[0].img }
-
+            imageData.value =
+                listOf(data1.await(), data2.await(), data3.await()).map { it.result[0].img }
             //更新加载状态
             loadState.value = LoadState.Success()
+        },
+            {
+                loadState.value = LoadState.Fail(it.message ?: "加载失败")
+            },
+            {
+                Log.d("xulimin","数据获取成功")
+            }
+        )
 
-        }
+
     }
 }
